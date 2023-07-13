a7979768c6c1294e5d4a7c70a4f214d646ffedcb
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-07-13 04:28:20 +0000

    adding custom fields

--- vehicle_repair_management/public/js/project.js
+++ vehicle_repair_management/public/js/project.js
@@ -2,6 +2,11 @@ frappe.ui.form.on('Project', {
     vehicle: function(frm) {
         if (!frm.doc.project_name) {
             frm.set_value('project_name', frm.doc.vehicle);
+
+            frappe.db.get_value('Vehicle', frm.doc.vehicle, 'customer')
+				.then(function (customer_name) {
+                    frm.set_value('customer', customer_name.message.customer);
+				});
         }
     }
 });
